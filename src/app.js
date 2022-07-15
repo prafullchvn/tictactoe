409f2fbf6b0ccf45f33263a1a5cc8ca7b33ffeb2
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2022-07-15 16:54:02 +0530

    Added middleware.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const { fileLogger } = require('./middleware/fileLogger.js');
+const { injectCookies } = require('./middleware/injectCookies.js');
+const { injectSession } = require('./middleware/injectSession.js');
 
 const createApp = (serveFrom) => {
   const app = express();
+  app.use(fileLogger);
+  app.use(injectCookies);
+  app.use(injectSession);
+
   app.use(express.static(serveFrom));
 
   return app;
